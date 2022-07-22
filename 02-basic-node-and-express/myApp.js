2388f40477f3555e6bf5b9cfc895e14933fac1bb
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-21 23:58:05 -0300

    05/12 - Serve JSON on a Specific Route

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -5,8 +5,8 @@ let app = express();
 // console.log("Hello World");
 
 /* 02 Answer - Start a Working Express Server*/
-/* app.get("/", (req, res) => {
-    res.send('Hello Express');
+/* app.get("/", (request, response) => {
+    response.send('Hello Express');
 }) */  
 
 /* 03 Answer - Server an HTML File*/
@@ -19,6 +19,13 @@ app.get("/", (request, response) => {
 /* 04 Answer - Serve Static Assets*/
 app.use("/public", express.static(__dirname + "/public"));
 
+/* 05 Answer - Serve JSON on a Specific Route*/
+app.get("/json", (request, response) => {
+  const data = {
+    "message": "Hello json"
+  };
+  response.json(data);
+})
 
 
 
