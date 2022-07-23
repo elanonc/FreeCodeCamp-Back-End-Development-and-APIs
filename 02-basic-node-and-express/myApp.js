cf4aeb9aa6f451b5d398a8b602f820bdea883762
Elano N. Caitano <nunes.elano14@gmail.com>
2022-07-23 12:25:25 -0300

    07/12 - Implement a Root-Level Request Logger Middleware

--- 02-basic-node-and-express/myApp.js
+++ 02-basic-node-and-express/myApp.js
@@ -2,25 +2,25 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-/* 01 Answer - Meet the Node console*/
+// 01 Answer - Meet the Node console
 // console.log("Hello World");
 
-/* 02 Answer - Start a Working Express Server*/
+// 02 Answer - Start a Working Express Server*/
 /* app.get("/", (request, response) => {
     response.send('Hello Express');
-}) */  
+}) 
 
-/* 03 Answer - Server an HTML File*/
+// 03 Answer - Server an HTML File
 /*app.get("/", (request, response) => { 
   const filePath = __dirname + '/views/index.html';
   console.log(filePath);
   response.sendFile(filePath);
 })*/
 
-/* 04 Answer - Serve Static Assets*/
+// 04 Answer - Serve Static Assets
 // app.use("/public", express.static(__dirname + "/public"));
 
-/* 05 Answer - Serve JSON on a Specific Route*/
+// 05 Answer - Serve JSON on a Specific Route
 /*app.get("/json", (request, response) => {
   const data = {
     "message": "Hello json"
@@ -28,13 +28,19 @@ let app = express();
   response.json(data);
 })*/
 
-app.get("/json", (req, res) => {
+// 06 Answer - Use the .env File
+/*app.get("/json", (req, res) => {
   const json = { message: "Hello json" };
   json.message = process.env.MESSAGE_STYLE === "uppercase" ? json.message.toUpperCase() : json.message;
   res.json(json);
 });
+*/
 
-
+// 07 - Implement a Root-Level Request Logger Middleware
+app.use((request, response, next) => {
+  console.log(`${request.method} ${request.path} - ${request.ip}`);
+  next();
+});
 
 
 
